45b53f7c559cf2b19cc510edc72d9f73c5ed54ac
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-05-24 00:52:15 -0700

    updated side nav, header, and landing phrase on home (index.tsx). Moving on to context, mongodb, and serversideprops integration

---
+++ src/context/tokenContext.tsx
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TokenContext = createContext({});
+
+const TokenProvider = ({ children }) => {
+  const [token, setToken] = useState({});
+
+  // useEffect(() => {
+  //   const dataFetcher = async () => {
+  //     axios
+  //       .get(`http://127.0.0.1:5000/`)
+  //       .then((res) => {
+  //         if (res.data.success && res.data.code == "returning_user") {
+  //           setDiscord(res.data.data)
+
+  //           // axios
+  //           //   .get(`/api/discord/refresh`)
+  //           //   .then(res2 => {
+  //           //     if (res2.data) {
+  //           //       console.log(res2.data)
+  //           //     }
+  //           //   })
+  //           //   .catch(err2 => {})
+  //         }
+  //       })
+  //       .catch((err) => {})
+  //   }
+
+  //   dataFetcher()
+  // }, [])
+
+  return (
+    <TokenContext.Provider value={[token, setToken]}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
+
+export { TokenContext, TokenProvider };
