f4758c40785fef852d0724b803a60bf5e4de2a2f
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-06-10 15:06:47 -0700

    built token chart, still needs work for mobile usage

--- src/components/tokens/components/TokenCharts/TokenChart.tsx
+++ src/components/tokens/components/TokenCharts/TokenChart.tsx
@@ -1,11 +1,8 @@
-import Card from '@components/common/Card';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
+import useWindowSize from '@hooks/useWindowSize';
 
 import {
-  Area,
-  AreaChart,
-  Label,
   LineChart,
   Line,
   CartesianGrid,
@@ -17,6 +14,7 @@ import {
 
 import styles from './TokenChart.module.scss';
 import useKeySwitch from './helpers/useKeySwitch';
+import Card from '@components/common/Card';
 import ButtonRow from '@components/common/ButtonRow';
 import Dropdown from '@components/common/Dropdown';
 
@@ -34,6 +32,10 @@ const initialState = {
 };
 
 export default function TokenChart({ props }) {
+  const { width } = useWindowSize();
+  const [chartWidth, setChartWidth] = useState(0);
+
+  const [noData, setNoData] = useState(false);
   const [state, setState] = useState(initialState);
   const [group, setGroup] = useState('Community');
   const [communityDropdownText, setCommunityDropdownText] =
@@ -61,7 +63,7 @@ export default function TokenChart({ props }) {
     function numberWithCommasDecimal(x) {
       return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
     }
-    console.log(payload);
+    // console.log(payload);
     if (active && payload && payload.length) {
       const day = moment(payload[0]['payload']['timestamp']).format('dddd');
       const date = moment(payload[0]['payload']['timestamp']).format('LL');
@@ -89,12 +91,47 @@ export default function TokenChart({ props }) {
     return null;
   };
 
+  const noDataChecker = () => {
+    const keys = key.split('.');
+
+    if (keys[1]) {
+      if (!props.tokenTimeseries[0].historical[0][keys[0]][keys[1]]) {
+        setNoData(true);
+      } else {
+        setNoData(false);
+      }
+    } else {
+      if (!props.tokenTimeseries[0].historical[0][keys[0]]) {
+        setNoData(true);
+      } else {
+        setNoData(false);
+      }
+    }
+  };
+  useEffect(() => {
+    noDataChecker();
+  }, [key]);
+
+  const NoDataComponent = () => {
+    const keys = key.split('.');
+
+    if (noData) {
+      return (
+        <div className={styles.no_data}>No data available at this time</div>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
+  useEffect(() => {
+    setChartWidth(width);
+  }, [width]);
+
   return (
     <Card>
-      <div
-        className="highlight-bar-charts"
-        style={{ userSelect: 'none', width: '100%' }}
-      >
+      <NoDataComponent />
+      <div className={styles.card_container}>
         <div className={styles.row_container}>
           <ButtonRow
             onClick={(item) => setGroup(item.text)}
@@ -105,6 +142,7 @@ export default function TokenChart({ props }) {
               { text: 'General' },
             ]}
           />
+
           <Dropdown
             selectedChild={
               group === 'Community'
@@ -158,47 +196,54 @@ export default function TokenChart({ props }) {
           />
         </div>
 
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart width={800} height={400} data={arr}>
-            <CartesianGrid vertical={false} stroke="white" />
+        {noData ? (
+          <div style={{ height: 400 }}></div>
+        ) : (
+          <ResponsiveContainer key={chartWidth} width="100%" height={400}>
+            <LineChart height={400} data={arr}>
+              <CartesianGrid vertical={false} stroke="white" />
 
-            <XAxis
-              dataKey="timestamp"
-              domain={[state.left, state.right]}
-              name="Time"
-              tickFormatter={(unixTime) => moment(unixTime).format('l')}
-              type="category"
-              stroke="white"
-            />
-            <YAxis
-              allowDataOverflow
-              domain={[state.bottom, state.top]}
-              type="number"
-              yAxisId="1"
-              stroke="white"
-            />
-            <Tooltip
-              content={<CustomTooltip />}
-              position={{ y: 0 }}
-              active={true}
-              cursor={{
-                strokeWidth: 5,
-                stroke: 'var(--green-10)',
-                strokeOpacity: 0.5,
-              }}
-            />
-            <Line
-              dot={<></>}
-              yAxisId="1"
-              type="natural"
-              dataKey={key}
-              stroke="gold"
-              strokeWidth={3}
-              animationDuration={1000}
-            />
-          </LineChart>
-        </ResponsiveContainer>
+              <XAxis
+                dataKey="timestamp"
+                domain={[state.left, state.right]}
+                name="Time"
+                tickFormatter={(unixTime) => moment(unixTime).format('l')}
+                type="category"
+                stroke="white"
+                padding={{ left: 40 }}
+              />
+              <YAxis
+                allowDataOverflow
+                domain={[state.bottom, state.top]}
+                type="number"
+                yAxisId="1"
+                stroke="white"
+                width={75}
+              />
+              <Tooltip
+                content={<CustomTooltip />}
+                position={{ y: 0 }}
+                active={true}
+                cursor={{
+                  strokeWidth: 5,
+                  stroke: 'var(--green-10)',
+                  strokeOpacity: 0.5,
+                }}
+              />
+              <Line
+                dot={<></>}
+                yAxisId="1"
+                type="natural"
+                dataKey={key}
+                stroke="gold"
+                strokeWidth={3}
+                animationDuration={0}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </div>
+      {/* </div> */}
     </Card>
   );
 }
