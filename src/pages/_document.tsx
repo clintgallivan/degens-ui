f2e416ba51c72293cdabb37e0d210fac571f3199
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-06-10 18:44:15 -0700

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap"
           rel="stylesheet"
