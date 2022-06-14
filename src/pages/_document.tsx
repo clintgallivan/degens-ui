e70a44b079d6cd72e138db6c0310f0700cdb1922
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-06-14 15:55:16 -0700

    created viewLeaderboards button on home screen token chart

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function MyDocument() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700;800;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
       </Head>
