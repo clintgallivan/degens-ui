80d7fd08429a2602e82056c031c85135d068f703
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-06-28 15:11:54 -0700

    fixed non-working links on token info on /token/<token> page. fixed token leaderboards to be working.

--- src/components/common/TopTokenTable/TopTokenTable.tsx
+++ src/components/common/TopTokenTable/TopTokenTable.tsx
@@ -5,6 +5,8 @@ import { BsAward } from 'react-icons/bs';
 
 import RetroButton from '@components/common/RetroButton';
 import styles from './TopTokenTable.module.scss';
+import Link from 'next/link';
+import { ImArrowDown2 } from 'react-icons/im';
 
 const TopTokenTable = ({ props }: any) => {
   const [selectedColumn, setSelectedColumn] = useState('by_degen_score');
@@ -15,53 +17,132 @@ const TopTokenTable = ({ props }: any) => {
     return `${src}?w=${width}&q=${quality || 75}`;
   };
 
+  // const RowHandler = () => {
+  //   let count = 0;
+  //   return topTokenData[selectedColumn].map((item: any) => {
+  //     if (count < 100) {
+  //       count++;
+  //       return (
+  //         <tr key={item.name} className={styles.row_container}>
+  //           <td>
+  //             <div className={styles.icon_container}>
+  //               <Image
+  //                 loader={imageLoader}
+  //                 src={item.image}
+  //                 alt=""
+  //                 height={24}
+  //                 width={24}
+  //               />
+  //             </div>
+  //           </td>
+  //           <td>
+  //             <div className={styles.item_name_container}>{item.name}</div>
+  //           </td>
+  //           <td>
+  //             <div className={styles.grid_point_container}>
+  //               {item.coingecko_score_rank}
+  //             </div>
+  //           </td>
+  //           <td>
+  //             <div className={styles.grid_point_container}>
+  //               {item.dev_score_rank}
+  //             </div>
+  //           </td>
+  //           <td>
+  //             <div className={styles.grid_point_container}>
+  //               {item.community_score_rank}
+  //             </div>
+  //           </td>
+  //           <td>
+  //             <div className={styles.grid_point_container}>
+  //               {item.liquidity_score_rank}
+  //             </div>
+  //           </td>
+  //         </tr>
+  //       );
+  //     } else {
+  //       return <></>;
+  //     }
+  //   });
+  // };
   const RowHandler = () => {
-    let count = 0;
-    return topTokenData[selectedColumn].map((item: any) => {
-      if (count < 100) {
-        count++;
+    return topTokenData[selectedColumn]
+      .slice(0, 100)
+      .map((item: any, index: number) => {
+        console.log(item);
         return (
           <tr key={item.name} className={styles.row_container}>
             <td>
-              <div className={styles.icon_container}>
-                <Image
-                  loader={imageLoader}
-                  src={item.image}
-                  alt=""
-                  height={24}
-                  width={24}
-                />
+              <div className={styles.icon_container}>#{index + 1}</div>
+            </td>
+            <td>
+              <div className={styles.icon_name_container}>
+                <div className={styles.icon_container}>
+                  <Image
+                    loader={imageLoader}
+                    src={item.image}
+                    alt=""
+                    height={24}
+                    width={24}
+                  />
+                </div>
+                <div className={styles.item_name_container}>
+                  <Link
+                    className={styles.a_tag}
+                    href={`/tokens/${item.coingecko_id}`}
+                  >
+                    <a className={styles.a_tag}>{item.name}</a>
+                  </Link>
+                </div>
               </div>
             </td>
             <td>
-              <div className={styles.item_name_container}>{item.name}</div>
-            </td>
-            <td>
-              <div className={styles.grid_point_container}>
+              <div
+                className={
+                  selectedColumn == 'by_degen_score'
+                    ? styles.grid_point_container && styles.selected_text
+                    : styles.grid_point_container && styles.unselected_text
+                }
+              >
                 {item.coingecko_score_rank}
               </div>
             </td>
             <td>
-              <div className={styles.grid_point_container}>
+              <div
+                className={
+                  selectedColumn == 'by_developer_score'
+                    ? styles.grid_point_container && styles.selected_text
+                    : styles.grid_point_container && styles.unselected_text
+                }
+              >
                 {item.dev_score_rank}
               </div>
             </td>
             <td>
-              <div className={styles.grid_point_container}>
+              <div
+                className={
+                  selectedColumn == 'by_community_score'
+                    ? styles.grid_point_container && styles.selected_text
+                    : styles.grid_point_container && styles.unselected_text
+                }
+              >
                 {item.community_score_rank}
               </div>
             </td>
             <td>
-              <div className={styles.grid_point_container}>
+              <div
+                className={
+                  selectedColumn == 'by_liquidity_score'
+                    ? styles.grid_point_container && styles.selected_text
+                    : styles.grid_point_container && styles.unselected_text
+                }
+              >
                 {item.liquidity_score_rank}
               </div>
             </td>
           </tr>
         );
-      } else {
-        return <></>;
-      }
-    });
+      });
   };
   return (
     <div className={styles.container}>
@@ -82,7 +163,22 @@ const TopTokenTable = ({ props }: any) => {
                 <RetroButton
                   onClick={() => setSelectedColumn('by_degen_score')}
                 >
-                  {<div className="fs-xsm fw-sb">Degen</div>}
+                  {
+                    <>
+                      {selectedColumn == 'by_degen_score' ? (
+                        <>
+                          <div className={styles.selected_col_header_text}>
+                            Degen
+                          </div>
+                          <ImArrowDown2 className={styles.arrow_icon} />
+                        </>
+                      ) : (
+                        <div className={styles.unselected_col_header_text}>
+                          Degen
+                        </div>
+                      )}
+                    </>
+                  }
                 </RetroButton>
               </div>
             </th>
@@ -91,7 +187,22 @@ const TopTokenTable = ({ props }: any) => {
                 <RetroButton
                   onClick={() => setSelectedColumn('by_developer_score')}
                 >
-                  {<div className="fs-xsm fw-sb">Developer</div>}
+                  {
+                    <>
+                      {selectedColumn == 'by_developer_score' ? (
+                        <>
+                          <div className={styles.selected_col_header_text}>
+                            Developer
+                          </div>
+                          <ImArrowDown2 className={styles.arrow_icon} />
+                        </>
+                      ) : (
+                        <div className={styles.unselected_col_header_text}>
+                          Developer
+                        </div>
+                      )}
+                    </>
+                  }
                 </RetroButton>
               </div>
             </th>
@@ -100,7 +211,22 @@ const TopTokenTable = ({ props }: any) => {
                 <RetroButton
                   onClick={() => setSelectedColumn('by_community_score')}
                 >
-                  {<div className="fs-xsm fw-sb">Community</div>}
+                  {
+                    <>
+                      {selectedColumn == 'by_community_score' ? (
+                        <>
+                          <div className={styles.selected_col_header_text}>
+                            Community
+                          </div>
+                          <ImArrowDown2 className={styles.arrow_icon} />
+                        </>
+                      ) : (
+                        <div className={styles.unselected_col_header_text}>
+                          Community
+                        </div>
+                      )}
+                    </>
+                  }
                 </RetroButton>
               </div>
             </th>
@@ -109,7 +235,22 @@ const TopTokenTable = ({ props }: any) => {
                 <RetroButton
                   onClick={() => setSelectedColumn('by_liquidity_score')}
                 >
-                  {<div className="fs-xsm fw-sb">Liquidity</div>}
+                  {
+                    <>
+                      {selectedColumn == 'by_liquidity_score' ? (
+                        <>
+                          <ImArrowDown2 className={styles.arrow_icon} />
+                          <div className={styles.selected_col_header_text}>
+                            Liquidity
+                          </div>
+                        </>
+                      ) : (
+                        <div className={styles.unselected_col_header_text}>
+                          Liquidity
+                        </div>
+                      )}
+                    </>
+                  }
                 </RetroButton>
               </div>
             </th>
@@ -119,6 +260,62 @@ const TopTokenTable = ({ props }: any) => {
       </Table>
     </div>
   );
+  // return (
+  //   <div className={styles.container}>
+  //     <Table>
+  //       <thead className={styles.head}>
+  //         <tr className={styles.head_row}>
+  //           <th>
+  //             <div className={styles.head_container}>
+  //               <BsAward size={25} />
+  //             </div>
+  //           </th>
+  //           <th className="fs-l">
+  //             <div className={styles.head_container}>Name</div>
+  //           </th>
+
+  //           <th>
+  //             <div className={styles.grid_point_container}>
+  //               <RetroButton
+  //                 onClick={() => setSelectedColumn('by_degen_score')}
+  //               >
+  //                 {<div className="fs-xsm fw-sb">Degen</div>}
+  //               </RetroButton>
+  //             </div>
+  //           </th>
+  //           <th>
+  //             <div className={styles.grid_point_container}>
+  //               <RetroButton
+  //                 onClick={() => setSelectedColumn('by_developer_score')}
+  //               >
+  //                 {<div className="fs-xsm fw-sb">Developer</div>}
+  //               </RetroButton>
+  //             </div>
+  //           </th>
+  //           <th>
+  //             <div className={styles.grid_point_container}>
+  //               <RetroButton
+  //                 onClick={() => setSelectedColumn('by_community_score')}
+  //               >
+  //                 {<div className="fs-xsm fw-sb">Community</div>}
+  //               </RetroButton>
+  //             </div>
+  //           </th>
+  //           <th>
+  //             <div className={styles.grid_point_container}>
+  //               <RetroButton
+  //                 onClick={() => setSelectedColumn('by_liquidity_score')}
+  //               >
+  //                 {<div className="fs-xsm fw-sb">Liquidity</div>}
+  //               </RetroButton>
+  //             </div>
+  //           </th>
+  //         </tr>
+  //       </thead>
+  //       <tbody className={styles.head}>{RowHandler()}</tbody>
+  //     </Table>
+  //   </div>
+  // );
 };
 
 export default TopTokenTable;
