7763464be350032aaa1661ce87de788e516dae42
Clint Gallivan <57740712+clintgallivan@users.noreply.github.com>
2022-07-12 11:24:11 -0700

    Update MultiSelectSearchBar.tsx

--- src/components/common/MultiSelectSearchBar/MultiSelectSearchBar.tsx
+++ src/components/common/MultiSelectSearchBar/MultiSelectSearchBar.tsx
@@ -1,15 +1,12 @@
 import Select from 'react-select';
 import styles from './MultiSelectSearchBar.module.scss';
-// import makeAnimated from 'react-select/animated';
 import { moduleStyles, ModuleTypes } from './moduleConfig';
-// import  from './moduleStyles';
 
 export default function MultiSelectSearchBar({
   placeholder,
   options,
   onChange,
 }) {
-  // const animatedComponents = makeAnimated();
   return (
     <Select
       closeMenuOnSelect={false}
