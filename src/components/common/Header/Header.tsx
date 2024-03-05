229c3c24208dd4d7c360b6ba150a807a2ac1b422
Clint Gallivan <clint@metalpay.co>
2024-03-04 22:46:58 -0800

    hotfix

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,6 +1,5 @@
 import styles from './Header.module.scss';
 import useWindowSize from '../../../hooks/useWindowSize';
-import { GoThreeBars } from 'react-icons/go';
 import { BiLogIn } from 'react-icons/bi';
 import RetroButton from '../RetroButton';
 import DegenLogo from '../DegenLogo';
@@ -9,33 +8,33 @@ import HeaderProfile from '../HeaderProfile';
 import HeaderSignIn from '../HeaderSignIn';
 
 export default function Header({ props }: any) {
-  const { width = 0 } = useWindowSize();
+    const { width = 0 } = useWindowSize();
 
-  return (
-    <>
-      <div className="header">
-        <DegenLogo />
-        <div
-          className={
-            props.session
-              ? width >= 480
-                ? styles.search_bar_authed_wide
-                : styles.search_bar_authed_narrow
-              : width >= 768
-              ? styles.search_bar_wide
-              : styles.search_bar_narrow
-          }
-        >
-          <HeaderSearchBar props={props} />
-        </div>
-        <div className={styles.profile}>
-          {props.session ? (
-            <HeaderProfile props={props} />
-          ) : (
-            <HeaderSignIn props={props} />
-          )}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="header">
+                <DegenLogo />
+                <div
+                    className={
+                        props.session
+                            ? width >= 480
+                                ? styles.search_bar_authed_wide
+                                : styles.search_bar_authed_narrow
+                            : width >= 768
+                            ? styles.search_bar_wide
+                            : styles.search_bar_narrow
+                    }
+                >
+                    <HeaderSearchBar props={props} />
+                </div>
+                <div className={styles.profile}>
+                    {props.session ? (
+                        <HeaderProfile props={props} />
+                    ) : (
+                        <HeaderSignIn props={props} />
+                    )}
+                </div>
+            </div>
+        </>
+    );
 }
